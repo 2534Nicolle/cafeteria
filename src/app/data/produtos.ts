b85ac656c3e01e2607f53f9fbc416c1961136f91
Nicolle <nicollegoncalves1331@gmail.com>
2025-09-02 10:27:04 -0300

    first commit

---
+++ src/app/data/produtos.ts
@@ -0,0 +1,22 @@
+export const boloDePote = [
+    {
+        title: "Cappuccino",
+        description: "Café expresso com leite cremoso e espuma.",
+        image: "cappuccino.webp"
+    },
+        {
+        title: "Mochaccino",
+        description: "Café com chocolate e leite gelado",
+        image: "mocha.avif"
+    },
+        {
+        title: "Pão de Queijo",
+        description: "Porção de clássico pão de queijo",
+        image: "paoQueijo.jpg"
+    },
+        {
+        title: "Bolo de Chocolate",
+        description: "Bolo de chocolate com cobertura de chocolate",
+        image: "boloChocolate.webp"
+    },
+]
